e0ef0bed2c6c5dcb7638bc52770efef9ae71af6b
b1f6c1c4 <b1f6c1c4@gmail.com>
2017-01-15 15:54:32 +0800

    uart

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,13 +31,17 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(148, 35);
+            this.button1.Location = new System.Drawing.Point(134, 44);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 138);
+            this.button1.Size = new System.Drawing.Size(68, 34);
             this.button1.TabIndex = 0;
             this.button1.Text = "start";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,33 +50,69 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(12, 35);
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(16, 44);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 88);
+            this.listBox1.Size = new System.Drawing.Size(95, 34);
             this.listBox1.TabIndex = 1;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(264, 35);
+            this.button2.Location = new System.Drawing.Point(231, 44);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(110, 138);
+            this.button2.Size = new System.Drawing.Size(75, 34);
             this.button2.TabIndex = 2;
             this.button2.Text = "stop";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(432, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(340, 53);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(65, 25);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Record";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(408, 233);
+            this.ClientSize = new System.Drawing.Size(1105, 1047);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,6 +121,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
 using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
 
 namespace Acq
 {
@@ -12,8 +16,13 @@ namespace Acq
 
         private StreamWriter m_StreamWriter;
 
-        private int m_Count;
         private readonly Profile m_Profile;
+
+        private int m_Count;
+
+        private int m_Frequency;
+
+        private Timer m_Timer;
 
         public Form1()
         {
@@ -21,6 +30,14 @@ namespace Acq
 
             foreach (var portName in SerialPort.GetPortNames())
                 listBox1.Items.Add(portName);
+            listBox1.Text = (string)listBox1.Items[0];
+
+            m_Timer = new Timer
+                          {
+                              Enabled = true,
+                              Interval = 1000,
+                          };
+            m_Timer.Tick += (s, o) => m_Frequency = Interlocked.Exchange(ref m_Count, 0);
 
             button2.Enabled = false;
 
@@ -32,7 +49,16 @@ namespace Acq
                                 Parity = Parity.None,
                                 StopBits = StopBits.One
                             };
-            m_Com = new AsyncSerialPort(m_Profile, 5);
+            m_Com = new AsyncSerialPort(m_Profile, 13 * 5 + 2);
+        }
+
+        private delegate void SetLabel1Delegate(string text);
+        private void SetLabel1(string text)
+        {
+            if (label1.InvokeRequired)
+                label1.Invoke(new SetLabel1Delegate(SetLabel1), text);
+            else
+                label1.Text = text;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -61,22 +87,113 @@ namespace Acq
 
         private bool ComOnPackageArrived(IList<byte> package)
         {
-            if (package[0] != 0x00)
+            if (package[0] != 0x5a)
+                return false;
+            if (package[1] != 0xa5)
                 return false;
 
-            var t = package[1];
-            t += package[2];
-            t += package[3];
-            t += package[4];
-            if (t != package[5])
-                return true;
+            var p = package.ToList();
+            p.RemoveRange(0, 2);
+            try
+            {
+                var sys = TakeF(p);
+                var state = TakeF(p);
 
-            var data = (package[1] << 24) | (package[2] << 16) | (package[3] << 8) | (package[4]);
-            m_StreamWriter.WriteLine(data);
+                var uACre = Take(p);
+                var uACim = Take(p);
+                var uACrms = Take(p);
+                var uACpha = Math.Atan2(uACim, uACre);
 
-            m_Count++;
+                var iACre = Take(p);
+                var iACim = Take(p);
+                var iACrms = Take(p);
+                var iACpha = Math.Atan2(iACim, iACre);
+
+                var lead = (iACpha - uACpha) % (Math.PI * 2);
+
+                var uDC = Take(p);
+                var impR = Take(p);
+                var impX = Take(p);
+                var impZ = Math.Sqrt(impR * impR + impX * impX);
+
+                var itACre = Take(p);
+                var itACim = Take(p);
+                var itACrms = Math.Sqrt(itACre * itACre + itACim * itACim);
+                var itACpha = Math.Atan2(itACim, itACre);
+
+                var tTanPhi = Take(p);
+                var tCosPhi = 1 / Math.Sqrt(1 + tTanPhi * tTanPhi);
+                var utDC = Take(p);
+
+                var sb = new StringBuilder();
+                Interlocked.Increment(ref m_Count);
+                sb.AppendLine($"Update frequency: {m_Frequency,10}Hz");
+                sb.AppendLine($"Sys Clk: {sys,10:x8}");
+                switch (state)
+                {
+                    case 0:
+                        sb.AppendLine("IDLE: All IGBT off");
+                        break;
+                    case 1:
+                        sb.AppendLine("IMP:  Impedance Measurement mode");
+                        break;
+                    case 2:
+                        sb.AppendLine("CURR: AC current closed loop");
+                        break;
+                    case 3:
+                        sb.AppendLine("FULL: DC voltage closed loop + AC curr.");
+                        break;
+                }
+                sb.AppendLine("Uac:");
+                sb.Append($"   {uACre,10:F6} + j{uACim,10:F6}V");
+                sb.AppendLine($"   RMS {uACrms,10:F6}V Phase {uACpha / Math.PI * 180,10:F6}");
+                sb.AppendLine("Iac:");
+                sb.Append($"   {iACre,10:F6} + j{iACim,10:F6}A");
+                sb.AppendLine($"   RMS {iACrms,10:F6}A Phase {iACpha / Math.PI * 180,10:F6}");
+                sb.AppendLine(
+                              lead > 0
+                                  ? $"Iac-Uac: Lead {+lead / Math.PI * 180,10:F6}"
+                                  : $"Iac-Uac: Lag  {-lead / Math.PI * 180,10:F6}");
+                sb.AppendLine($"Udc: {uDC,10:F6}V");
+                sb.AppendLine("Imp:");
+                sb.Append($"   {impR,10:F6} + j{impX,10:F6}Ohm");
+                sb.AppendLine($"   = {impZ,10:F6}Ohm");
+                sb.AppendLine("Target Iac:");
+                sb.Append($"   {itACre,10:F6} + j{itACim,10:F6}A");
+                sb.AppendLine($"   RMS {itACrms,10:F6}A Phase {itACpha / Math.PI * 180,10:F6}");
+                sb.Append(
+                          tTanPhi > 0
+                              ? $"Target cos phi: (Lead) {tCosPhi,10:F6}"
+                              : $"Target cos phi: (Lag)  {tCosPhi,10:F6}");
+                sb.AppendLine($"    tan phi={tTanPhi,10:F6}");
+                sb.AppendLine($"Target Udc: {utDC,10:F6}");
+
+                SetLabel1(sb.ToString());
+                if (checkBox1.Checked)
+                    ; //m_StreamWriter.WriteLine($"{iACre}");
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
 
             return true;
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            m_Com.Send(Encoding.ASCII.GetBytes(textBox1.Text));
+            textBox1.Text = string.Empty;
+        }
+
+        private static int TakeF(List<byte> package)
+        {
+            var data = (package[0] << 24) | (package[1] << 16) | (package[2] << 8) | (package[3]);
+            package.RemoveRange(0, 4);
+
+            return data;
+        }
+
+        private static double Take(List<byte> package) => TakeF(package) / Math.Pow(2, 20);
     }
 }
